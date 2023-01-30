fcf8ca52c061e6e5ccb93c1db71f4635d4ef0776
张瑞 <123930505+zhang690819@users.noreply.github.com>
2023-01-30 16:46:29 +0800

    two commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    host: "localhost",
+    open: true,
+    proxy: "http://localhost:2999"
+}
+})
